7c23ab2aa339e3f789163a4ce4ce35ba9d5e1f5a
Arvind Kumar <arvindkum8312@gmail.com>
2026-01-17 23:17:41 +0530

    admin route changes

--- routes/admin.js
+++ routes/admin.js
@@ -2,50 +2,86 @@ const { Router } = require("express");
 const adminRouter = Router();
 const { adminModel, courseModel } = require("../db");
 const jwt = require("jsonwebtoken");
+const { z } = require("zod");
+const bcrypt = require("bcrypt");
 // brcypt, zod, jsonwebtoken
 const  { JWT_ADMIN_PASSWORD } = require("../config");
 const { adminMiddleware } = require("../middleware/admin");
 
 
 adminRouter.post("/signup", async function(req, res) {
-    const { email, password, firstName, lastName } = req.body; // TODO: adding zod validation
-    // TODO: hash the password so plaintext pw is not stored in the DB
+    //zod validation
+     const requiredBody = z.object({
+            email: z.string().email(),
+            password: z.string().min(6),
+            firstName: z.string(),
+            lastName: z.string(),
+        });
+
+    const parsedData = requiredBody.safeParse(req.body);
+    
+    if(!parsedData.success){
+        return res.status(400).json({
+            message: "Invalid request data",
+            error: parsedData.error.errors
+        });
+    }
+
+    const { email, password, firstName, lastName } = req.body; 
+
+    const hashedPassword = await bcrypt.hash(password, 5);
 
     // TODO: Put inside a try catch block
-    await adminModel.create({
-        email: email,
-        password: password,
-        firstName: firstName, 
-        lastName: lastName
-    })
-    
-    res.json({
-        message: "Signup succeeded"
-    })
+    try {
+        await adminModel.create({
+            email,
+            password: hashedPassword,
+            firstName,
+            lastName
+        })
+        
+        res.json({
+            message: "Signup succeeded"
+        })
+    } catch (error) {
+        res.status(500).json({
+            message: "Error creating admin",
+            error: error.message
+        })
+    }
 })
 
 adminRouter.post("/signin", async function(req, res) {
-    const { email, password } = req.body;
+    const { email, password } = req.body;
 
     // TODO: ideally password should be hashed, and hence you cant compare the user provided password and the database password
-    const admin = await adminModel.findOne({
-        email: email,
-        password: password
-    });
+    const admin = await adminModel.findOne({ email });
+    const passwordMatch = bcrypt.compare(password, admin.password);
 
-    if (admin) {
+    if(!admin && !passwordMatch){
+        return res.status(403).json({
+            message: "Incorrect credentials"
+        });
+    }
+
+    try {
         const token = jwt.sign({
             id: admin._id
         }, JWT_ADMIN_PASSWORD);
 
-        // Do cookie logic
+        //cookie logic
+        res.cookie("token", token, {
+            httpOnly: true,
+            maxAge: 24 * 60 * 60 * 1000 // 1 day
+        });
 
         res.json({
             token: token
         })
-    } else {
-        res.status(403).json({
-            message: "Incorrect credentials"
+    } catch (error) {
+        res.status(500).json({
+            message: "Error signing in admin",
+            error: error.message
         })
     }
 })
